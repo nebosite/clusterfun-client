a2c988a75ae42527e42d2ed3f318b48e8f0cf80f
TheHans255 <hans@thehans255.com>
2022-10-27 19:42:41 -0700

    WIP: Add random walk tests
    - TODO: Fix test failures - path search is taking us through enemy paths

--- src/games/Lexible/models/LetterGridPathFinder.spec.ts
+++ src/games/Lexible/models/LetterGridPathFinder.spec.ts
@@ -1,7 +1,21 @@
 import { expect } from "chai";
+import { Vector2 } from "libs";
 import { LetterGridModel } from "./LetterGridModel";
 import { LetterGridPathFinder } from "./LetterGridPathFinder";
-import { LexibleScoredWordMessage } from "./Messages";
+
+function generateUniformlyFilledGrid(width: number, height: number, 
+    team: string, score: string, nextRandom: () => number) {
+    const LETTERS = "QWERTYUIOPASDFGHJKLZXCVBNM";
+    let letterDeck = "";
+    for (let i = 0; i < width * height; i++) {
+        letterDeck += LETTERS.charAt(Math.floor(LETTERS.length * nextRandom()));
+        letterDeck += team;
+        letterDeck += score;
+    }
+    const grid = new LetterGridModel(width, height);
+    grid.populate(letterDeck);
+    return grid;
+}
 
 function generateFullyRandomGrid(width: number, height: number, nextRandom: () => number) {
     const LETTERS = "QWERTYUIOPASDFGHJKLZXCVBNM";
@@ -23,26 +37,21 @@ describe("LetterGridPathFinder tests", () => {
         const GRID_WIDTH = 7;
         const GRID_HEIGHT = 8;
         const testCases = [
-            ["A", "_0", "neutral"],
-            ["B", "_0", "neutral"],
-            ["A", "A3", "ally"],
-            ["B", "A3", "enemy"],
-            ["A", "B3", "enemy"],
-            ["B", "B3", "ally"],
+            ["A", "_", "neutral"],
+            ["B", "_", "neutral"],
+            ["A", "A", "ally"],
+            ["B", "A", "enemy"],
+            ["A", "B", "enemy"],
+            ["B", "B", "ally"],
         ]
         const gridDescription: Record<string, string> = {
-            "_0": "a fully empty grid",
-            "A3": "a grid conqueured by team A",
-            "B3": "a grid conqueured by team B"
+            "_": "a fully empty grid",
+            "A": "a grid conqueured by team A",
+            "B": "a grid conqueured by team B"
         }
         for (const testCase of testCases) {
             it(`Identifies ${gridDescription[testCase[1]]} as needing an all ${testCase[2]} path for team ${testCase[0]}`, () => {
-                let letterDeck = "";
-                for (let i = 0; i < GRID_WIDTH * GRID_HEIGHT; i++) {
-                    letterDeck += "A" + testCase[1];
-                }
-                const grid = new LetterGridModel(GRID_WIDTH, GRID_HEIGHT);
-                grid.populate(letterDeck);
+                const grid = generateUniformlyFilledGrid(GRID_WIDTH, GRID_HEIGHT, testCase[1], "4", Math.random);
                 const finder = new LetterGridPathFinder();
                 const path = finder.findHotPath(grid, testCase[0] as "A" | "B");
                 expect(path.nodes.length).equals(GRID_WIDTH);
@@ -63,12 +72,52 @@ describe("LetterGridPathFinder tests", () => {
         const GRID_WIDTH_VARIANCE = 100;
         const GRID_HEIGHT_VARIANCE = 100;
         for (let i = 0; i < 10; i++) {
-            const width = MIN_GRID_WIDTH + Math.floor(Math.random() * GRID_WIDTH_VARIANCE);
-            const height = MIN_GRID_HEIGHT + Math.floor(Math.random() * GRID_HEIGHT_VARIANCE);
-            const grid = generateFullyRandomGrid(width, height, Math.random);
-            const finder = new LetterGridPathFinder();
-            const team = Math.random() < 0.5 ? "A" : "B";
-            finder.findHotPath(grid, team);
+            it(`Doesn't crash (iteration ${i + 1})`, () => {
+                const width = MIN_GRID_WIDTH + Math.floor(Math.random() * GRID_WIDTH_VARIANCE);
+                const height = MIN_GRID_HEIGHT + Math.floor(Math.random() * GRID_HEIGHT_VARIANCE);
+                const grid = generateFullyRandomGrid(width, height, Math.random);
+                const finder = new LetterGridPathFinder();
+                const team = Math.random() < 0.5 ? "A" : "B";
+                finder.findHotPath(grid, team);
+            });
+        }
+    })
+
+    describe("Random walk tests", () => {
+        const MIN_GRID_WIDTH = 5;
+        const MIN_GRID_HEIGHT = 5;
+        const GRID_WIDTH_VARIANCE = 10;
+        const GRID_HEIGHT_VARIANCE = 10;
+        for (let i = 0; i < 10; i++) {
+            it(`Finds winning path if it exists (iteration ${i + 1}`, () => {
+                const width = MIN_GRID_WIDTH + Math.floor(Math.random() * GRID_WIDTH_VARIANCE);
+                const height = MIN_GRID_HEIGHT + Math.floor(Math.random() * GRID_HEIGHT_VARIANCE);
+                const grid = generateUniformlyFilledGrid(width, height, "_", "0", Math.random);
+                const team = i % 2 === 0 ? "A" : "B";
+    
+                let x = 0, y = Math.floor(height / 2);
+                while (x < width) {
+                    const xRand = Math.random();
+                    const yRand = Math.random();
+                    if (xRand > 0.6) x++;
+                    //else if (xRand < 0.2) x--;
+                    if (yRand > 0.7) y++;
+                    else if (yRand < 0.3) y--;
+                    if (x < 0) x = 0;
+                    if (y < 0) y = 0;
+                    if (y > height - 1) y = height - 1;
+                    if (x < width) {
+                        const block = grid.getBlock(new Vector2(x, y))!;
+                        block.setScore(4, team);
+                    }
+                }
+                console.log(grid.serialize());
+                const finder = new LetterGridPathFinder();
+                const path = finder.findHotPath(grid, team);
+                expect(path.cost.neutral).equals(0);
+                expect(path.cost.enemy).equals(0);
+                expect(path.nodes.length).equals(path.cost.ally);
+            });
         }
     })
 });
