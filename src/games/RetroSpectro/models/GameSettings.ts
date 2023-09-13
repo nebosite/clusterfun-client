4c6cd127a2f41f4cc3e4ba5c2b34b512ab7fb472
Eric Jorgensen <ericjorg@thejcrew.net>
2023-09-13 16:48:51 -0700

    RetroSpectro v0.5.0

--- src/games/RetroSpectro/models/GameSettings.ts
+++ src/games/RetroSpectro/models/GameSettings.ts
@@ -1,4 +1,4 @@
 export const SETTING_ANSWER_CHARACTER_LIMIT = 60;
 export const ANSWER_STAGE_TIME_MS = 180000;
-export const RetroSpectroVersion = "0.4.1"
+export const RetroSpectroVersion = "0.5.0"
 
